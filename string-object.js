ee46f103c242a34186adb6f083095b40680afd87
abusayed0ne <0ne.abusayed@gmail.com>
2024-09-26 21:05:42 +0600

    AddedNew

---
+++ string-object.js
@@ -0,0 +1,72 @@
+// const country = "Bangladesh";
+// const division = "NewTown";
+// const district = new String("Dhaka");
+
+// const thana = new String("Demra",1234);
+// const delta = new String("Howto");
+
+// console.log(typeof(delta));
+// console.log(thana);
+// console.log(typeof(country));
+// console.log(typeof(district));
+
+
+// const numbers  = [1,2,3,4,5];
+// console.log(numbers.length);
+// console.log(typeof(numbers));
+// console.log(numbers[1]);
+
+// const capital = "Dhaka";
+// console.log(capital.length);
+// console.log(capital[1]);
+
+// const school = "HAFJGAJ GAJHG Ajshsg jahjgahag";
+// const school2 = new Object("Hawa")
+// const school3 = new String("Hawa")
+// console.log(typeof(school2));
+// console.log(typeof(school3));
+// console.log(school.toUpperCase());
+// console.log(school.toLowerCase());
+
+// const drink = ' Water';
+// const liquid = 'Water ';
+
+// if(drink.trim()===liquid.trim()){
+//     console.log("Abc");
+
+// }
+// else{
+//     console.log("ELC")
+
+// let text = " Hi There";
+// console.log(text.trim());
+
+// const address = 'andorkilla';
+// const part = address.slice(2,5);
+// console.log(part);
+// console.log(address.slice(2,5));
+
+// const sentence = 'jafhaj ahjghajg hajhhajg a';
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+// const friendsStr = ['Rahim,Karim,Boro,Chandu'];
+// console.log(friendsStr.join());
+// console.log(friendsStr.join('|'));
+// console.log(friendsStr.join('-'));
+
+
+// const realFriend = ['Hasan','Kamal','Mahim','Rahana'];
+// console.log(realFriend.join());
+// console.log(realFriend.join('|'));
+// console.log(realFriend.join('-'));
+
+
+const first = "Abid";
+const last = "Naba";
+
+const fullName = first + " "+  last;
+const fullName1 = first.concat("-","|",last.concat("-")) ;
+// console.log(fullName);
+console.log(fullName1);
+console.log(fullName1.length);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-for(let i = 1; i < 10; i++){
-    if(i%2 ===1){
-        continue;
-    }
-    console.log(i)
-}
+
