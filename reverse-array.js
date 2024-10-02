0e1e7b38d402b6bc363d9508d04df8528cb29f71
abusayed0ne <0ne.abusayed@gmail.com>
2024-10-02 23:43:48 +0600

    Practice'

--- reverse-array.js
+++ reverse-array.js
@@ -23,9 +23,24 @@ const abc = numbers.reverse(); // main array
 // }
 // console.log(reversed_numbers);
 
-const rev_num = [];
-for (let i = numbers.length - 1; i > 0 ; i --){
-    const num = numbers[i];
-    rev_num.push(num);
-}
-console.log(rev_num);
+// const rev_num = [];
+// for (let i = numbers.length - 1; i > 0 ; i --){
+//     const num = numbers[i];
+//     rev_num.push(num);
+// }
+// console.log(rev_num);
+
+
+
+// const ns = [1,2,3,4,5,6,7,8];
+// let rev = [];
+// for (let i = ns.length - 1; i>0 ; i-- ){
+//     rev.push(ns[i]);
+// }
+// console.log(rev);
+
+
+const num = [1,2,3,4,5,6,7];
+for(i = num.length-1; i >= 0 ; i--){
+    console.log(num[i]);
+};
