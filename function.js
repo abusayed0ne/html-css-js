de19f847814cea107d17af092b3b1fe1ac05b88b
abusayed0ne <0ne.abusayed@gmail.com>
2024-10-10 00:19:41 +0600

    Funcation

--- function.js
+++ function.js
@@ -1,8 +1,13 @@
-//Function Declare 
-function userInterface(){
-    console.log("Hi There! How was your day");
-    console.log("Is everything al right today!")
-}
-//Call the function 
-userInterface();
+// //Function Declare 
+// function userInterface(){
+//     console.log("Hi There! How was your day");
+//     console.log("Is everything al right today!")
+// }
+// //Call the function 
+// userInterface();
 
+// function fanOffKor(){
+//     console.log("Off ki hoise!");
+//     console.log("Off na hoile taratari off kor bhai");
+// }
+//  fanOffKor();
