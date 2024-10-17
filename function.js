b40bd06a15ba1b1b08261fba022cfaf35fc5ec35
abusayed0ne <0ne.abusayed@gmail.com>
2024-10-18 00:51:24 +0600

    newly added

--- function.js
+++ function.js
@@ -11,3 +11,50 @@
 //     console.log("Off na hoile taratari off kor bhai");
 // }
 //  fanOffKor();
+
+// function paramchek(num){
+//     console.log("Check the number: ",num*num);
+// }
+
+// paramchek(2);
+// function abc(n1,n2){
+//     diff= n2 - n1;
+//     console.log("The difference: ", diff);
+
+// }
+// const a= 20;
+// const b = 30;
+
+// abc(a,b);
+
+// function tentimes(n){
+//     num = n * 10;
+    
+    
+// }
+// b = tentimes(2);
+// console.log(b)
+
+
+// function add(p1,p2){
+//     const total = p1 + p2;
+//     return total;
+// }
+//  const bill = add(5,10);
+ 
+//  function add2(p1,p2){
+//     return p1 + p2;
+//  }
+//  const bill2 = add(5,10);
+ 
+//  console.log(bill,bill2)
+
+function doMath(n1,n2){
+    const s = n1 + n2;
+    const d = n1 - n2; 
+    const m = s * d; 
+    const r = m / 2; 
+    return r;
+}
+const abc = doMath(20,10);
+console.log(abc)
