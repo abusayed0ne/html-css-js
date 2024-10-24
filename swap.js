8c480cd41aa4b3fcf2da67eff0431c6c0e4c331d
abusayed0ne <0ne.abusayed@gmail.com>
2024-10-24 09:52:04 +0600

    swapping added

---
+++ swap.js
@@ -0,0 +1,20 @@
+// let a = 5; 
+// let b = 7; 
+
+// console.log(a,b);
+
+// const temp = a; 
+// a = b; 
+// b = temp; 
+
+// console.log(a,b);
+
+//-----------------------------
+
+let x = 5;
+let y = 7;
+
+console.log(x,y);
+[x,y] = [y,x]; // Destructing 
+
+console.log(x,y);
